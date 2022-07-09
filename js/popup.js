fd2ef35e431070a7326f63508c5ec6497a569893
trannguyenhan <trannguyenhan01092000@gmail.com>
2022-07-09 16:34:05 +0700

    refacter style code

--- js/popup.js
+++ js/popup.js
@@ -1,29 +1,34 @@
 (() => { 
     // state variables
     let toDoListArray = [];
+    
     // ui variables
     const form = document.querySelector(".form"); 
     const input = form.querySelector(".form__input");
     const ul = document.querySelector(".toDoList"); 
   
-    // event listeners
     form.addEventListener('submit', e => {
       // prevent default behaviour - Page reload
       e.preventDefault();
+
       // give item a unique ID
       let itemId = String(Date.now());
+
       // get/assign input value
       let toDoItem = input.value;
+
       //pass ID and item into functions
       addItemToDOM(itemId , toDoItem);
       addItemToArray(itemId, toDoItem);
+
       // clear the input box. (this is default behaviour but we got rid of that)
       input.value = '';
     });
     
     ul.addEventListener('click', e => {
       let id = e.target.getAttribute('data-id')
-      if (!id) return // user clicked in something else      
+      if (!id) return // user clicked in something else     
+
       //pass id through to functions
       removeItemFromDOM(id);
       removeItemFromArray(id);
@@ -34,8 +39,10 @@
       // create an li
       const li = document.createElement('li')
       li.setAttribute("data-id", itemId);
+
       // add toDoItem text to li
       li.innerText = toDoItem
+
       // add li to the DOM
       ul.appendChild(li);
     }
@@ -49,6 +56,7 @@
     function removeItemFromDOM(id) {
       // get the list item by data ID
       var li = document.querySelector('[data-id="' + id + '"]');
+      
       // remove list item
       ul.removeChild(li);
     }
